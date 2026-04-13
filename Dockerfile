29600842b16d46c117adce347db75bbd6e1ef9f0
luiz <luizfelipedpt@gmail.com>
2026-04-12 22:33:11 -0300

    feat: backend fastapi

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-From python:3.10.19-slim-trixie
-ENV PYTHONUNBUFFERED 1
-WORKDIR /app
---
+++ backend/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+import json
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+redis = get_redis_connection(
+    host="localhost",
+    port=6379,
+    password="senharedis",
+    decode_responses=True
+)
+
+class Delivery(HashModel):
+    budget: int = 0
+    notes: str = ""
+
+    class Meta:
+        database = redis
+
+class Event(HashModel):
+    delivery_id: str = ""
+    type: str
+    data: str
+
+    class Meta:
+        database = redis
+
+@app.post('/deliveries/create')
+async def create(request: Request):
+    body = await request.json()
+    delivery = Delivery(budget=body['data']['budget'], notes=body['data']['notes'])
+    return delivery
